78af8744b1414a454673beb3bfd975850cd25a6e
son <sonbon1101@gmail.com>
2023-08-09 22:39:52 +0700

    fix code

--- src/server/index.js
+++ src/server/index.js
@@ -7,18 +7,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // You could call it aylienapi, or anything else
-var textapi = new aylien({
+const textapi = new aylien({
   application_id: process.env.API_ID,
   application_key: process.env.API_KEY,
 });
 
-console.log(`Your API key is ${process.env.API_KEY}`);
-
 const app = express();
 
 app.use(express.static("dist"));
-
-console.log(__dirname);
 
 app.get("/", function (req, res) {
   res.sendFile("dist/index.html");
@@ -27,6 +23,24 @@ app.get("/", function (req, res) {
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
   console.log("Example app listening on port 8080!");
+});
+
+app.post("/testApi", function (req, res, next) {
+  try {
+    const data = textapi.sentiment(
+      {
+        text: req.body.text,
+      },
+      (error, response) => {
+        if (error === null) {
+          res.status(200).send(response);
+        }
+      }
+    );
+  } catch (err) {
+    console.log("Error: " + err);
+    return res.status(400).json({ messages: "Invalid response" });
+  }
 });
 
 app.get("/test", function (req, res) {
