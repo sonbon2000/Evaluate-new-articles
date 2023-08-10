aaae83bc1d275895edc7638e03bc9609a7ddc49b
son <sonbon1101@gmail.com>
2023-08-10 20:12:08 +0700

    fix comment

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,8 +1,22 @@
 function handleSubmit(event) {
   event.preventDefault();
   // check what text was put into the form field
-  let formText = document.getElementById("name").value;
+  const formText = document.getElementById("name").value;
+  const results = document.querySelector("#results");
   Client.checkForName(formText);
   console.log("::: Form Submitted :::");
+
+  let data = {
+    text: formText,
+  };
+
+  fetch("http://localhost:8080/testApi", {
+    method: "POST",
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      results.textContent = data.text;
+    });
 }
 export { handleSubmit };
