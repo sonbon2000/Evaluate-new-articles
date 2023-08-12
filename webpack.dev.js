088791236b3614512f3ba4b3c2af3ae807532edd
son <sonbon1101@gmail.com>
2023-08-12 18:47:20 +0700

    fix comment

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,26 +2,23 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
-  mode: "development",
-  devtool: "source-map",
   output: {
     libraryTarget: "var",
     library: "Client",
   },
-  stats: "verbose",
+  mode: "development",
   module: {
     rules: [
       {
         test: "/.js$/",
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: "babel_loader",
       },
       {
-        test: /.scss$/,
+        test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
@@ -32,17 +29,10 @@ module.exports = {
       filename: "./index.html",
     }),
     new CleanWebpackPlugin({
-      // Simulate the removal of files
       dry: true,
-      // Write Logs to Console
       verbose: true,
-      // Automatically remove all unused webpack assets on rebuild
       cleanStaleWebpackAssets: true,
       protectWebpackAssets: false,
-    }),
-    new WorkboxPlugin.GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true,
     }),
   ],
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -8,24 +8,24 @@ const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
-  mode: "production",
-  optimization: {
-    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
-  },
   output: {
     libraryTarget: "var",
     library: "Client",
+  },
+  mode: "production",
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
   },
   module: {
     rules: [
       {
         test: "/.js$/",
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: "babel_loader",
       },
       {
-        test: /.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
@@ -34,10 +34,9 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
-    new MiniCssExtractPlugin({ filename: "[name].css" }),
-    new WorkboxPlugin.GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true,
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
     }),
+    new WorkboxPlugin.GenerateSW(),
   ],
 };
